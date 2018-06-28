d1d6b4032e3125901c7b88432804753e9b1387bf
Gabriela <anagabrielamacsim@gmail.com>
2018-06-28 16:49:35 +0300

    added css&modified js

--- catclicker.js
+++ catclicker.js
@@ -1,1 +1,14 @@
+var catImage = document.getElementById('pisica');
 
+
+function getCurrentNrOfClicks() {
+  return Number.parseInt($("#clicks").text());
+}
+
+function listenToCat () {
+  let x = getCurrentNrOfClicks();
+  x = x + 1;
+  $('#clicks').text(x)
+}
+
+catImage.addEventListener('click', listenToCat);
