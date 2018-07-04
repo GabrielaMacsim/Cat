6bf6ff78a9a85345313e4cef45bd1243fa42dcad
Gabriela <anagabrielamacsim@gmail.com>
2018-07-04 13:01:13 +0300

    implemented the same functionality

--- catclicker.js
+++ catclicker.js
@@ -1,14 +1,23 @@
-var catImage = document.getElementById('pisica');
+var catImage1 = document.getElementById('pisica1');
+var catImage2 = document.getElementById('pisica2');
 
-
-function getCurrentNrOfClicks() {
-  return Number.parseInt($("#clicks").text());
+function getCurrentNrOfClicks1() {
+  return Number.parseInt($("#clicks1").text());
+}
+function getCurrentNrOfClicks2() {
+  return Number.parseInt($("#clicks2").text());
 }
 
-function listenToCat () {
-  let x = getCurrentNrOfClicks();
+function listenToCat1 () {
+  let x = getCurrentNrOfClicks1();
   x = x + 1;
-  $('#clicks').text(x)
+  $('#clicks1').text(x)
+}
+function listenToCat2 () {
+  let y = getCurrentNrOfClicks2();
+  y = y + 1;
+  $('#clicks2').text(y)
 }
 
-catImage.addEventListener('click', listenToCat);
+catImage1.addEventListener('click', listenToCat1);
+catImage2.addEventListener('click', listenToCat2);
